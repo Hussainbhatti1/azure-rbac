6ac0dfb64a973388816c560144d1e4c4c617aac0
Hussain Bhatti <hussain.nuces@gmail.com>
2025-02-13 00:40:09 -0500

    Code refactoring

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+# Fetch the current subscription data
+data "azurerm_subscription" "example" {
+  # This automatically pulls the subscription ID from the authenticated Azure account
+}
+
 resource "azurerm_role_definition" "example_role" {
   name        = "example_role"
   description = "A custom role"
@@ -13,16 +18,13 @@ resource "azurerm_role_definition" "example_role" {
     not_actions = []
   }
 
-  # Add the scope where the role will be assigned
+  # Assignable scopes: the subscription where the role can be applied
   assignable_scopes = ["/subscriptions/${data.azurerm_subscription.example.id}"]
 }
 
 resource "azurerm_role_assignment" "example_assignment" {
   principal_id        = "29ec2766-fb35-4c65-858f-04237669591d"
   role_definition_name = azurerm_role_definition.example_role.name
+  # Role assignment scope: the subscription to which the role is assigned
   scope                = "/subscriptions/${data.azurerm_subscription.example.id}"
-}
-
-data "azurerm_subscription" "example" {
-  # This automatically pulls the subscription ID from the authenticated Azure account
 }
