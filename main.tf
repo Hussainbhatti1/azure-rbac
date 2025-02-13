b80b90a8f1634fa6b0eab0d486634edd9342a013
Hussain Bhatti <hussain.nuces@gmail.com>
2025-02-13 00:35:53 -0500

    Code refactoring

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  features {}
 }
 
 resource "azurerm_role_definition" "example_role" {
@@ -15,11 +12,15 @@ resource "azurerm_role_definition" "example_role" {
     ]
     not_actions = []
   }
-  assignable_scopes = ["/subscriptions/${var.subscription_id}"]
+  assignable_scopes = ["/subscriptions/${data.azurerm_subscription.example.id}"]
 }
 
 resource "azurerm_role_assignment" "example_assignment" {
-  principal_id   = "29ec2766-fb35-4c65-858f-04237669591d"
+  principal_id        = "29ec2766-fb35-4c65-858f-04237669591d"
   role_definition_name = azurerm_role_definition.example_role.name
-  scope          = "/subscriptions/${var.subscription_id}"
+  scope                = "/subscriptions/${data.azurerm_subscription.example.id}"
+}
+
+data "azurerm_subscription" "example" {
+  # This automatically pulls the subscription ID from the authenticated Azure account
 }
